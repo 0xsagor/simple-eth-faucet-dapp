4ed4e329a8677d29beb229b0bfb94e5f80230684
Sagor <161376518+0xsagor@users.noreply.github.com>
2026-02-18 15:24:39 +0600

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Faucet = await hre.ethers.getContractFactory("Faucet");
+  const faucet = await Faucet.deploy();
+
+  await faucet.waitForDeployment();
+
+  console.log("Faucet deployed to:", await faucet.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
